73bb9fc468bf930377544b4fbabf427b5b4dd779
assaf mazor <assaf.mazor@gmail.com>
2023-06-03 21:52:36 +0300

    ...

--- js/db/builders/data-builder.js
+++ js/db/builders/data-builder.js
@@ -7,7 +7,9 @@ function DataBuilder() {
     "https://psychometric-cloud.github.io/assets/questions/MALLO/2022/a/data.json",
     "https://psychometric-cloud.github.io/assets/questions/MALLO/2022/sp/data.json",
     "https://psychometric-cloud.github.io/assets/questions/MALLO/2022/su/data.json",
-    "https://psychometric-cloud.github.io/assets/questions/MALLO/2022/w/data.json"
+    "https://psychometric-cloud.github.io/assets/questions/MALLO/2022/w/data.json",
+    "https://psychometric-cloud.github.io/assets/questions/MALLO/2021/w/data.json",
+    "https://psychometric-cloud.github.io/assets/questions/MALLO/2021/su/data.json",
   ];
 
   //-----------------------------------------
