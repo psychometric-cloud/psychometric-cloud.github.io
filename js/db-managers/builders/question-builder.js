84731aa463eeb76aee0a6613008d025d416ae3c0
assaf mazor <assaf.mazor@gmail.com>
2023-06-25 08:57:33 +0300

    ...

--- js/db-managers/builders/question-builder.js
+++ js/db-managers/builders/question-builder.js
@@ -1,18 +1,21 @@
 function QuestionBuilder() {
 
   let add = (test, question, chapter) => {
-    let qInfo = question.split(":");
-    let areas = qInfo[1].split(",");
 
-    qBank.push({
-      publisher: test.publisher,
-      year: parseInt(test.year),
-      season: test.season,
-      chapter: chapter,
-      qNum: parseInt(qInfo[0]),
-      qAreas: areas,
-      aNum: parseInt(qInfo[2])
-    });
+    if (question !== "skip") {
+      let qInfo = question.split(":");
+      let areas = qInfo[1].split(",");
+
+      qBank.push({
+        publisher: test.publisher,
+        year: parseInt(test.year),
+        season: test.season,
+        chapter: chapter,
+        qNum: parseInt(qInfo[0]),
+        qAreas: areas,
+        aNum: parseInt(qInfo[2])
+      });
+    }
   }
 
   return {
