2a9fccfe408108af5fcf38426976ff7fd86954d5
assaf mazor <assaf.mazor@gmail.com>
2023-07-26 17:16:41 +0300

    ...

--- files.js
+++ files.js
@@ -39,12 +39,16 @@ window.urlsList = [
   "https://psychometric-cloud.github.io/assets/questions/MALLO/2014/oct/data.json",
   "https://psychometric-cloud.github.io/assets/questions/MALLO/2014/jul/data.json",
 
-
   //----------------------------------------
-  "https://psychometric-cloud.github.io/assets/questions/MALLO/2014/apr/data.json",
+
+  "https://psychometric-cloud.github.io/assets/questions/MALLO/2014/apr/data.json", //OK
   "https://psychometric-cloud.github.io/assets/questions/MALLO/2014/feb/data.json", //OK
   "https://psychometric-cloud-part2.github.io/assets/questions/MALLO/2013/dec/data.json", //OK
   "https://psychometric-cloud-part2.github.io/assets/questions/MALLO/2013/sep/data.json", //OK
   "https://psychometric-cloud-part2.github.io/assets/questions/MALLO/2013/jul/data.json", //OK
   "https://psychometric-cloud-part2.github.io/assets/questions/MALLO/2013/apr/data.json", //OK
+
+  //----------------------------------------
+
+  // Hi Assaf. Put new files here......
 ];
