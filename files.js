a56fd120df2731fcd1cea4123d1e73d409bfedea
assaf mazor <assaf.mazor@gmail.com>
2023-07-26 09:48:11 +0300

    ..

--- files.js
+++ files.js
@@ -43,5 +43,8 @@ window.urlsList = [
   //----------------------------------------
   "https://psychometric-cloud.github.io/assets/questions/MALLO/2014/apr/data.json",
   "https://psychometric-cloud.github.io/assets/questions/MALLO/2014/feb/data.json",
-  
+  "https://psychometric-cloud-part2.github.io/assets/questions/MALLO/2013/dec/data.json",
+  "https://psychometric-cloud-part2.github.io/assets/questions/MALLO/2013/sep/data.json",
+  "https://psychometric-cloud-part2.github.io/assets/questions/MALLO/2013/jul/data.json",
+  "https://psychometric-cloud-part2.github.io/assets/questions/MALLO/2013/apr/data.json",
 ];
