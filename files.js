d205235e81e9726171befc935482c8e0342162a1
assaf mazor <assaf.mazor@gmail.com>
2023-08-01 17:13:11 +0300

    ...

--- files.js
+++ files.js
@@ -28,44 +28,47 @@ window.urlsList = [
 
   "https://psychometric-cloud.github.io/assets/questions/MALLO/2017/dec/data.json", //ok
   "https://psychometric-cloud.github.io/assets/questions/MALLO/2017/sep/data.json", //ok
-  "https://psychometric-cloud.github.io/assets/questions/MALLO/2017/feb/data.json",
+  "https://psychometric-cloud.github.io/assets/questions/MALLO/2017/feb/data.json", //ok
   "https://psychometric-cloud.github.io/assets/questions/MALLO/2017/apr/data.json", //ok
   "https://psychometric-cloud.github.io/assets/questions/MALLO/2017/jul/data.json", //ok
 
-  "https://psychometric-cloud.github.io/assets/questions/MALLO/2016/dec/data.json",
-  "https://psychometric-cloud.github.io/assets/questions/MALLO/2016/apr/data.json",
-  "https://psychometric-cloud.github.io/assets/questions/MALLO/2016/sep/data.json",
-  "https://psychometric-cloud.github.io/assets/questions/MALLO/2016/jul/data.json",
-  "https://psychometric-cloud.github.io/assets/questions/MALLO/2016/feb/data.json",
+  "https://psychometric-cloud.github.io/assets/questions/MALLO/2016/dec/data.json", //ok
+  "https://psychometric-cloud.github.io/assets/questions/MALLO/2016/apr/data.json", //ok
+  "https://psychometric-cloud.github.io/assets/questions/MALLO/2016/sep/data.json", //ok
+  "https://psychometric-cloud.github.io/assets/questions/MALLO/2016/jul/data.json", //ok
+  "https://psychometric-cloud.github.io/assets/questions/MALLO/2016/feb/data.json", //ok
+
   "https://psychometric-cloud.github.io/assets/questions/MALLO/2015/dec/data.json",
   "https://psychometric-cloud.github.io/assets/questions/MALLO/2015/sep/data.json",
   "https://psychometric-cloud.github.io/assets/questions/MALLO/2015/jul/data.json",
   "https://psychometric-cloud.github.io/assets/questions/MALLO/2015/apr/data.json",
   "https://psychometric-cloud.github.io/assets/questions/MALLO/2015/feb/data.json",
+
   "https://psychometric-cloud.github.io/assets/questions/MALLO/2014/dec/data.json",
   "https://psychometric-cloud.github.io/assets/questions/MALLO/2014/oct/data.json",
   "https://psychometric-cloud.github.io/assets/questions/MALLO/2014/jul/data.json",
-
-  //----------------------------------------
-
   "https://psychometric-cloud.github.io/assets/questions/MALLO/2014/apr/data.json",
   "https://psychometric-cloud.github.io/assets/questions/MALLO/2014/feb/data.json",
+
   "https://psychometric-cloud-part2.github.io/assets/questions/MALLO/2013/dec/data.json",
   "https://psychometric-cloud-part2.github.io/assets/questions/MALLO/2013/sep/data.json",
   "https://psychometric-cloud-part2.github.io/assets/questions/MALLO/2013/jul/data.json",
   "https://psychometric-cloud-part2.github.io/assets/questions/MALLO/2013/apr/data.json",
   "https://psychometric-cloud-part2.github.io/assets/questions/MALLO/2013/feb/data.json",
+
   "https://psychometric-cloud-part2.github.io/assets/questions/MALLO/2012/dec/data.json",
   "https://psychometric-cloud-part2.github.io/assets/questions/MALLO/2012/oct/data.json",
   "https://psychometric-cloud-part2.github.io/assets/questions/MALLO/2012/jul/data.json",
   "https://psychometric-cloud-part2.github.io/assets/questions/MALLO/2012/apr/data.json",
   "https://psychometric-cloud-part2.github.io/assets/questions/MALLO/2012/feb/data.json",
   "https://psychometric-cloud-part2.github.io/assets/questions/MALLO/2012/PS1H/data.json",
+
   "https://psychometric-cloud-part2.github.io/assets/questions/MALLO/2011/dec/data.json",
   "https://psychometric-cloud-part2.github.io/assets/questions/MALLO/2011/oct/data.json",
   "https://psychometric-cloud-part2.github.io/assets/questions/MALLO/2011/jul/data.json",
   "https://psychometric-cloud-part2.github.io/assets/questions/MALLO/2011/apr/data.json",
   "https://psychometric-cloud-part2.github.io/assets/questions/MALLO/2011/feb/data.json",
+
   "https://psychometric-cloud-part2.github.io/assets/questions/MALLO/2010/dec/data.json",
   "https://psychometric-cloud-part2.github.io/assets/questions/MALLO/2010/oct/data.json",
   "https://psychometric-cloud-part2.github.io/assets/questions/MALLO/2010/jul/data.json",
