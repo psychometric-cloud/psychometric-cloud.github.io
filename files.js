ad3cd37844ed21ae8d8f2b20bbbd3afee2438e31
assaf mazor <assaf.mazor@gmail.com>
2023-08-08 11:01:55 +0300

    ...

--- files.js
+++ files.js
@@ -84,9 +84,8 @@ window.urlsList = [
 
   "https://psychometric-cloud-part2.github.io/assets/questions/MALLO/2009/apr/data.json", //ok
   "https://psychometric-cloud-part2.github.io/assets/questions/MALLO/2009/feb/data.json", //ok
-  /*
-    "https://psychometric-cloud-part2.github.io/assets/questions/onexone/s1/data.json", todo - idk what the season or year to put!!!
-    "https://psychometric-cloud-part2.github.io/assets/questions/onexone/s2/data.json",todo - idk what the season or year to put!!!
-    "https://psychometric-cloud-part2.github.io/assets/questions/onexone/s3/data.json", todo - idk what the season or year to put!!!
-    */
+
+  "https://psychometric-cloud-part2.github.io/assets/questions/onexone/s1/data.json",
+  "https://psychometric-cloud-part2.github.io/assets/questions/onexone/s2/data.json",
+  //"https://psychometric-cloud-part2.github.io/assets/questions/onexone/s3/data.json"
 ];
