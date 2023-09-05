2bdc9895388cf33e33d65a8398965c0600980dee
assaf mazor <assaf.mazor@gmail.com>
2023-09-05 10:54:21 +0300

    ...

--- files.js
+++ files.js
@@ -89,7 +89,9 @@ window.urlsList = [
   "https://psychometric-cloud-part2.github.io/assets/questions/onexone/s3/data.json", //ok
   "https://psychometric-cloud-part2.github.io/assets/questions/onexone/s4/data.json", //ok
 
-  "https://psychometric-cloud-part2.github.io/assets/questions/nivr/s6/data.json", //ok
+
+  "https://psychometric-cloud-part2.github.io/assets/questions/nivr/s1/data.json", //ok 
   "https://psychometric-cloud-part2.github.io/assets/questions/nivr/s2/data.json", //ok 
-  "https://psychometric-cloud-part2.github.io/assets/questions/nivr/s1/data.json" //ok 
+  "https://psychometric-cloud-part2.github.io/assets/questions/nivr/s4/data.json", //ok 
+  "https://psychometric-cloud-part2.github.io/assets/questions/nivr/s6/data.json" //ok
 ];
