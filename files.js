1a199354db441bd433d719f28b7ec2d1e90ec790
assaf mazor <assaf.mazor@gmail.com>
2023-11-01 14:37:48 +0200

    ...

--- files.js
+++ files.js
@@ -92,6 +92,7 @@ window.urlsList = [
 
   "https://psychometric-cloud-part2.github.io/assets/questions/nivr/s1/data.json", //ok 
   "https://psychometric-cloud-part2.github.io/assets/questions/nivr/s2/data.json", //ok 
+  "https://psychometric-cloud-part2.github.io/assets/questions/nivr/s3/data.json", //ok 
   "https://psychometric-cloud-part2.github.io/assets/questions/nivr/s4/data.json", //ok 
   "https://psychometric-cloud-part2.github.io/assets/questions/nivr/s6/data.json", //ok
   "https://psychometric-cloud-part2.github.io/assets/questions/nivr/s7/data.json", //ok
