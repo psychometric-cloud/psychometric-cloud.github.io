36c5fa276d370a638a1fb0a0d86921e43cf23eb9
shaul mazor <shaul.mazor@gmail.com>
2024-07-02 14:48:35 +0300

    ...

--- js/test-managers/test-data-builder.js
+++ js/test-managers/test-data-builder.js
@@ -11,7 +11,7 @@ function TestDataBuilder() {
       selectedAreas: [],
       selectedSubject: "math",
       testQuestions: 15,
-      quizQuestions: 2
+      quizQuestions: 10
     },
     {
       selectedAreas: ['complete', 'restate'],
